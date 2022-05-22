7533cf2ee5bf406dde96a5e1de445744a136f29b
KeeYoo <poronko2045@gmail.com>
2022-05-22 15:13:00 +0300

    Old logger is written.

---
+++ LoggerAdvanced/LoggerAdvanced/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerAdvanced
+{
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
